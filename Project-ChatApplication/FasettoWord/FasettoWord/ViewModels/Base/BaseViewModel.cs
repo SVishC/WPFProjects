a381c67e092d46a7580a8e47b1d54b8450f777f0
SVishC <chaitanyamvishnu@gmail.com>
2022-08-04 19:38:42 +0530

    Added 1.BaseViewmodel and 2.Relay Command which can be extended by all other VMs in future.

---
+++ Project-ChatApplication/FasettoWord/FasettoWord/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+
+namespace FasettoWord
+{
+
+    /// <summary>
+    /// A base view model that all view models implents .This view model has propety changed and hence fires the event as and when needed.
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name">Property should pass on its name</param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
